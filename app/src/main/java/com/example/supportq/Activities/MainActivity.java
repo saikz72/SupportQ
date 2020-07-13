e565838dca34eae33759900545a2db17d86183f8
Saikou Ceesay <saikz@fb.com>
2020-07-13 14:11:31 -0700

    all 4 fragments initialized and connected to the bottom navigation

--- app/src/main/java/com/example/supportq/Activities/MainActivity.java
+++ app/src/main/java/com/example/supportq/Activities/MainActivity.java
@@ -10,6 +10,10 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.supportq.Fragments.ComposeFragment;
+import com.example.supportq.Fragments.HomeFragment;
+import com.example.supportq.Fragments.InboxFragment;
+import com.example.supportq.Fragments.ProfileFragment;
 import com.example.supportq.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -31,35 +35,35 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.action_compose:
                         //TODO: update fragment
-                        //fragment = new ComposeFragment();
+                        fragment = new ComposeFragment();
                         Toast.makeText(MainActivity.this, "compose", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_home:
                         //TODO: update fragment
-                       // fragment = new HomeFragment();
+                        fragment = new HomeFragment();
                         Toast.makeText(MainActivity.this, "home", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_profile:
                         //TODO: update fragment
-                        //fragment = new ProfileFragment();
+                        fragment = new ProfileFragment();
                         Toast.makeText(MainActivity.this, "profile", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.action_mail:
                         //TODO: update fragment
-                        //fragment = new MailFragment();
+                        fragment = new InboxFragment();
                         Toast.makeText(MainActivity.this, "mail", Toast.LENGTH_SHORT).show();
                         break;
                     default:
-                       // fragment = new HomeFragment();
+                        fragment = new HomeFragment();
                         Toast.makeText(MainActivity.this, "home", Toast.LENGTH_SHORT);
 
                         break;
                 }
-                //fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
-        //bottomNavigationView.setSelectedItemId(R.id.action_home);
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 
     public void setViews() {
---
+++ app/src/main/java/com/example/supportq/Fragments/InboxFragment.java
@@ -0,0 +1,37 @@
+package com.example.supportq.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.supportq.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link InboxFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class InboxFragment extends Fragment {
+
+
+    public InboxFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inbox, container, false);
+    }
+}
