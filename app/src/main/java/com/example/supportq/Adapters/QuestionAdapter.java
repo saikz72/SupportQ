30eb41adac8f551f537aa555a3e63b5ac24e3b15
Saikou Ceesay <saikz@fb.com>
2020-07-14 15:42:38 -0700

    card view added

--- app/src/main/java/com/example/supportq/Adapters/QuestionAdapter.java
+++ app/src/main/java/com/example/supportq/Adapters/QuestionAdapter.java
@@ -1,12 +1,16 @@
 package com.example.supportq.Adapters;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.supportq.Models.Question;
@@ -44,14 +48,25 @@ public class QuestionAdapter extends RecyclerView.Adapter<QuestionAdapter.ViewHo
 
     class ViewHolder extends RecyclerView.ViewHolder {
         private TextView tvDescription;
+        private CardView card;
+        private ImageView ivLike;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvDescription = itemView.findViewById(R.id.tvDescription);
-            //tvDescription.setVisibility(View.GONE);
+            card = itemView.findViewById(R.id.card);
+            ivLike = itemView.findViewById(R.id.ivLike);
         }
 
         public void bind(Question question) {
             tvDescription.setText(question.getDescription());
+            card.setCardBackgroundColor(Color.parseColor("#E6E6E6"));
+            card.setMaxCardElevation(0.0f);
+            card.setRadius(5.0f);
+            ivLike.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                }
+            });
         }
     }
     // Clean all elements of the recycler
--- app/src/main/java/com/example/supportq/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/supportq/Fragments/ProfileFragment.java
@@ -48,6 +48,8 @@ public class ProfileFragment extends Fragment {
         // TODO -- redesign profile layout
         editProfileButtonClicked();
 
+        logoutButtonClicked();
+
 
     }
 
