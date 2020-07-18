891c07285a06ceac921c3da3fa74d40192b2fadd
Saikou Ceesay <saikz@fb.com>
2020-07-18 16:26:19 -0700

    login and sign up screen redesign

--- app/src/main/java/com/example/supportq/Activities/LoginActivity.java
+++ app/src/main/java/com/example/supportq/Activities/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.supportq.Models.ProgressIndicator;
@@ -18,7 +19,7 @@ import com.parse.ParseFacebookUtils;
 import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button btnLogin;
+    private ImageView btnLogin;
     private Button btnSignUp;
     private Button btnSignIn;
     private TextInputLayout username_input_text;
