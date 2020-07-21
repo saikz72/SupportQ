bd409ad8da7586461b2b099e50ea14d019af83e9
Saikou Ceesay <saikz@fb.com>
2020-07-21 08:42:48 -0700

    app logo added on the toolbar of home and profile fragment

--- app/src/main/java/com/example/supportq/Adapters/ReplyAdapter.java
+++ app/src/main/java/com/example/supportq/Adapters/ReplyAdapter.java
@@ -12,7 +12,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
-import com.example.supportq.Models.Question;
 import com.example.supportq.Models.Reply;
 import com.example.supportq.Models.User;
 import com.example.supportq.R;
