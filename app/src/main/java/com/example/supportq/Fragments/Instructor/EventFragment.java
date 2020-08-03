4f942c9d90486997b8b4b865d035187980f667d2
Saikou Ceesay <saikz@fb.com>
2020-08-03 03:09:45 -0700

    date picker implemented in EventFragment

--- app/src/main/java/com/example/supportq/Fragments/Instructor/EventFragment.java
+++ app/src/main/java/com/example/supportq/Fragments/Instructor/EventFragment.java
@@ -1,18 +1,28 @@
 package com.example.supportq.Fragments.Instructor;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.example.supportq.R;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+
 public class EventFragment extends Fragment {
+    Button btnDatePicker;
+    TextView tvDate;
 
     public EventFragment() {
         // Required empty public constructor
@@ -26,5 +36,29 @@ public class EventFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setViews(view);
+        final DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+                tvDate.setText(currentDate);
+            }
+        };
+        btnDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment datePicker = new DatePickerFragment(mDateSetListener);
+                datePicker.show(getFragmentManager(), "date picker");
+            }
+        });
+    }
+
+    public void setViews(View views) {
+        btnDatePicker = views.findViewById(R.id.btnDatePicker);
+        tvDate = views.findViewById(R.id.tvDate);
     }
 }
